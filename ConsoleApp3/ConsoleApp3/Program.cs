2ebf938f22ec415c5df5a66d7d82108a2faa6e66
Gazimjanov Egor <Egar.net2000@gmail.com>
2025-05-29 18:37:48 +0600

    Add ex1

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3
+{
+    public delegate string WorkStr();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Lamp lamp = new Lamp();
+            Man man = new Man();
+
+            WorkStr s = cat.WhatIsDoingNow;
+            s += lamp.GetLampStatus;
+            s += man.WhatHeIsDoingNow;
+
+            for (int i = 0; i < s.GetInvocationList().Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {s.GetInvocationList()[i].DynamicInvoke()}");
+            }
+        }
+    }
+}
